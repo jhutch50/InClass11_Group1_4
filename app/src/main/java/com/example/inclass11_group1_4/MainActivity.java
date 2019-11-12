ade2f25497109210fa57b7a13f1c59a7f91610ac
Joshua Hutcheson <jhutch50@uncc.edu>
2019-11-11 20:08:59 -0500

    Adding from code

--- app/src/main/java/com/example/inclass11_group1_4/MainActivity.java
+++ app/src/main/java/com/example/inclass11_group1_4/MainActivity.java
@@ -1,14 +1,158 @@
-package com.example.inclass11_group1_4;
+package com.example.inclass11_group1_4.;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String TAG = "demo";
+
+    ImageView iv_TakePhoto;
+    Button buttonUpload;
+    ImageView iv_uploadedPhoto;
+    ProgressBar progressBar;
+    Bitmap bitmapUpload = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_TakePhoto = findViewById(R.id.iv_TakePhoto);
+
+        iv_TakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        buttonUpload = findViewById(R.id.buttonUpload);
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage(getBitmapCamera());
+            }
+        });
+
+        progressBar = findViewById(R.id.progressBar);
+        iv_uploadedPhoto = findViewById(R.id.iv_uploadedPhoto);
+    }
+
+    private Bitmap getBitmapCamera() {
+        //If photo not taken from camera...
+        if (bitmapUpload == null){
+            return ((BitmapDrawable) iv_TakePhoto.getDrawable()).getBitmap();
+        }
+//        Photo taken from camera...
+        return bitmapUpload;
+    }
+
+
+    //    Upload Camera Photo to Cloud Storage....
+    private void uploadImage(Bitmap photoBitmap){
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
+
+        final StorageReference imageRepo = storageReference.child("images/camera.png");
+
+//        Converting the Bitmap into a bytearrayOutputstream....
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        photoBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = imageRepo.putBytes(data);
+//        uploadTask.addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.e(TAG, "onFailure: "+e.getMessage());
+//            }
+//        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                Log.d(TAG, "onSuccess: "+"Image Uploaded!!!");
+//            }
+//        });
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+//                return null;
+                if (!task.isSuccessful()){
+                    throw task.getException();
+                }
+
+                return imageRepo.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()){
+                    Log.d(TAG, "Image Download URL"+ task.getResult());
+                    String imageURL = task.getResult().toString();
+
+                    Picasso.get().load(imageURL).into(iv_uploadedPhoto);
+                }
+            }
+        });
+
+//        ProgressBar......
+
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                progressBar.setProgress((int) progress);
+                System.out.println("Upload is " + progress + "% done");
+            }
+        });
+
+    }
+
+
+    //    TAKE PHOTO USING CAMERA...
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        Camera Callback........
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            iv_TakePhoto.setImageBitmap(imageBitmap);
+
+            bitmapUpload = imageBitmap;
+        }
     }
 }
